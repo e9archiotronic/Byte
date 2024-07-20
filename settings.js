f331567a411593a94e11ce2943a33220533bad4a
e9archiotronic <e9@archiotronic.com>
2024-07-20 11:43:31 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = 'Powered by Hamza';
 global.owner = process.env.OWNER_NUMBER  || '923072380380' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'XByte=vU0GETQB#lnzWrPnT1RlblYqCIsKY87JZNb2Vg7LAbyoaGMAVQAg' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "923072380380" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
